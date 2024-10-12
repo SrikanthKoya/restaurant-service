ce59ba769a66067d83f00687d84f92afa0e815bf
Srikanth Koya <srikanth.koyas@gmail.com>
2024-10-12 21:23:31 +0800

    first commit

---
+++ src/test/java/com/codeddecode/restaurantlisting/RestaurantListingApplicationTests.java
@@ -0,0 +1,10 @@
+package com.codeddecode.restaurantlisting;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantListingApplicationTests {
+
+
+}
---
+++ src/test/java/com/codedecode/restaurantlisting/RestaurantlistingApplicationTests.java
@@ -0,0 +1,12 @@
+// package com.codedecode.restaurantlisting;
+
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class RestaurantlistingApplicationTests {
+
+// 	// @Test
+// 	// void contextLoads() {
+// 	// }
+
+// }
